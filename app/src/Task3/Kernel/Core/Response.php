20d8e793ce46bcde14747315e65f36c3e377c494
kolotov <25168302+kolotov@users.noreply.github.com>
2022-02-19 16:17:30 +0200

    faet: implement response

--- app/src/Task3/Kernel/Core/Response.php
+++ app/src/Task3/Kernel/Core/Response.php
@@ -4,7 +4,67 @@ declare(strict_types=1);
 
 namespace App\Task3\Kernel\Core;
 
+/**
+ * Client response
+ */
 class Response
 {
+    private array $headers;
+    private string $content;
+    private int $status;
 
+    public function __construct(?string $content = '', int $status = 200, array $headers = [])
+    {
+        $this->headers = $headers;
+        $this->setContent($content);
+        $this->setStatusCode($status);
+    }
+
+    /**
+     * @return array
+     */
+    public function getHeaders(): array
+    {
+        return $this->headers;
+    }
+
+    /**
+     * @param array $headers
+     */
+    public function setHeaders(array $headers): void
+    {
+        $this->headers = $headers;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatusCode(): int
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param int $status
+     */
+    public function setStatusCode(int $status): void
+    {
+        $this->status = $status;
+    }
 }
